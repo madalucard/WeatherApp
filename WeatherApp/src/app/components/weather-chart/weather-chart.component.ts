a41ed380f9120f4b2724e5b86f16b3f760a389a1
Martin Lapihuska <madalucard@gmail.com>
2024-07-11 09:11:40 +0200

    Base table + input for line chart

--- WeatherApp/src/app/components/weather-chart/weather-chart.component.ts
+++ WeatherApp/src/app/components/weather-chart/weather-chart.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-weather-chart',
   templateUrl: './weather-chart.component.html',
-  styleUrl: './weather-chart.component.scss'
+  styleUrl: './weather-chart.component.scss',
 })
 export class WeatherChartComponent {
-
+  @Input() chartData: any;
 }
